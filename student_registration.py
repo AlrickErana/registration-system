7a26d1f6757f550db80285d40200464368a29e65
ricky <AlrickErana>
2024-04-06 19:36:09 +0800

    first commit

---
+++ student_registration.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from datetime import date
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+from tkinter.ttk import Combobox
+import openpyxl, xlrd
+from openpyxl import workbook
+import pathlib
+
+
+root = Tk()
+root.title("student Registration DB")
+root.geometry("1180x700+210+100")
+root.config()
+
+
+
+root.mainloop()
